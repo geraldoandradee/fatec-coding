b1edea5dd9388bc089cabadeb44aff0a8305c4e8
Geraldo Andrade <geraldo@geraldoandrade.com>
2014-04-23 21:12:33 -0300

    Operacoes com ponteiros

---
+++ programacao-micro-computadores/c/aritmetica_de_ponteiros.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	int a[10];
+	int * p;
+	int i = 0;
+
+	p = &a[0];
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("p + %d = %p\n", i, p+i);
+		*(p + i) = 0;
+	}
+	printf("Conteudo do vetor: \n");
+
+	for (i = 0; i < 10; ++i)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	printf("\b\b.\n");
+
+	return 0;
+}
