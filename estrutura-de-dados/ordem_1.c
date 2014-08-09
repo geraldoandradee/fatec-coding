d678ddbee718f8e572e9cb01c90e632edba31192
Geraldo Andradee <geraldo@geraldoandrade.com>
2014-08-09 14:27:18 -0300

    Finalized queue ordering

--- estrutura-de-dados/ordem_1.c
+++ estrutura-de-dados/ordem_1.c
@@ -5,27 +5,26 @@
 
 int busca(int *, int, int); //protótipo
 void desloca(int *, int, int); //protótipo
+void mostra(int *, int, char *); //protótipo
 
 int main () {
 	int v[10] = {2, 4, 6, 8, 0, 0, 0, 0, 0, 0};
 	int novo, pos_novo, ult = 3;
+	mostra(v, ult, "Vetor original");
 	printf("Digite um novo elemento: ");
 	scanf("%d", &novo);  // joga a referencia na variavel novo
-
 	pos_novo = busca(v, ult, novo);
-	desloca(v, ult, novo);
+	desloca(v, ult, pos_novo);
 	v[pos_novo] = novo; // nova posicao pode receber o valor do item
 	ult++; // atualiza a posicao do ultimo item
-
+	mostra(v, ult, "Vetor alterado");
 	return 0;
 }
 
 
 int busca(int *v, int ult, int novo){
 	int i;
-	
 	for (i=0; i<=ult && v[i] < novo;i++);
-	
 	return i;	
 }
 
@@ -35,4 +34,13 @@ void desloca(int *v, int ult, int pos_novo) {
 	for (i=ult+1;i>pos_novo;i--) {
 		v[i] = v[i-1];
 	}
+}
+
+void mostra(int * v, int ult, char * msg) {
+	int i;
+	printf("%s\n", msg);
+	for (i=0;i<=ult;i++) {
+		printf("%d ", v[i]);
+	}
+	printf("\n");
 }
