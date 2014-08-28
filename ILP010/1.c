cd96361d2e7976135bdd60337c0d52617cc08c16
Geraldo Andradee <geraldo@geraldoandrade.com>
2014-08-27 21:03:33 -0300

    Parcial exercicios

---
+++ ILP010/1.c
@@ -0,0 +1,15 @@
+//1. Escreva um programa que receba uma string do teclado e informe quantas letras maiúsculas ela possui.
+
+// A diferenca inteira entre um char minusculo e maiusculo eh 32. A=65, a=97. 97 - 65 = 32
+
+#include <stdio.h>
+
+int main() {
+	char string_para_contar[]; 
+	int tamanho_da_string;
+	printf("Digite uma string qualquer: ");
+	scanf("%14[^\n]", &string_para_contar);
+
+
+	return 0;
+}
