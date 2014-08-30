d70a6b1558727202ba9ce72e89a9c819aacf6b80
Geraldo Andrade <geraldo@geraldoandrade.com>
2014-08-29 21:24:35 -0300

    Parcial: geracao de vetores OK

--- estrutura-de-dados/trabalho 1/trabalho1.c
+++ estrutura-de-dados/trabalho 1/trabalho1.c
@@ -1,10 +1,77 @@
 # include "trabalho1.h"   // INCLUI A BIBLIOTECA ou HEADER trabalho.h A EXECUCAO DO PROGRAMA
 
+
+int main() {
+	printf("\nGerando o vetor aleatorio...\n");
+	int vetor_aleatorio[T];
+	int vetor_desc[T];
+	int vetor_asc[T];
+
+	gera(vetor_aleatorio, T);
+	printf("Vetor aleatorio gerado \n");
+
+	gera_vetor_decrescente(vetor_desc, T);
+	printf("Vetor descrescente gerado \n");
+//
+//	gera_vetor_crescente(vetor_asc, T);
+//	printf("Vetor crescente gerado \n");
+
+//
+//	int comparacaoBubble = 0;
+//	int ordenacaoBubble = 0;
+//
+//	printf("Agora vamos ordenar o vetor em ordem crescente");
+//	int comparacaoInsertion = 0;
+//	int ordenacaoInsertion = 0;
+//
+//	printf("Agora vamos ordenar o vetor em ordem decrescente");
+//
+//	printf("Agora vamos ordenar o vetor de modo crescente");
+	return 0;
+}
+
+void gera_vetor_decrescente(int * vetor, int n) {
+	int i=0;
+	for(i=n;i>0;--i) {
+		vetor[i] = i;
+	}
+}
+
+void gera_vetor_crescente(int * vetor, int n) {
+	int i=0;
+	for(i=0;i<n;i++) {
+		vetor[i] = i;
+	}
+}
+
+/*
+ * Essa funcao testa se o item já existe na matriz
+ *
+ * int v: matriz de vetores
+ * int n: numero de itens
+ * int numero_gerado: pa
+ **/
+int tem_repeticao(int * v, int n, int numero_gerado) {
+	int i = 0;
+	for (i=0;i<n;++i) {
+		if (v[i] == numero_gerado) {
+			return 1; // ja existe
+		}
+	}
+	return 0;  // nao tem repeticao, urru
+}
+
 /* FUNCAO UTILIZADA PARA GERAR UM VETOR DE N�MEROS ALEAT�RIOS */
 void gera (int *v, int n){
-	int i;
-	for ( i= 0; i < n; i++ ){
-		v[i] = rand();
+	int i=0;
+	int numero;
+
+	while(i<n) {
+		numero = rand();
+		if (!tem_repeticao(v, n, numero)) {
+			v[i] = numero;
+			++i;
+		}
 	}
 }
 
--- estrutura-de-dados/trabalho 1/trabalho1.h
+++ estrutura-de-dados/trabalho 1/trabalho1.h
@@ -2,18 +2,20 @@
 # include <stdlib.h>
 # include <time.h>
 
-# define T 100		// CONSTANTE DEFINIDA PARA A QUANTIDADE DE ITENS GERADOS NO VETOR
+# define T 100000		// CONSTANTE DEFINIDA PARA A QUANTIDADE DE ITENS GERADOS NO VETOR
 
 void gera (int *, int);		// FUNCAO CRIADA PARA GERAR O VETOR
 void mostra (int *, int, char *);	// FUNCAO CRIADA PARA MOSTRAR O VETOR GERADO
 void mostraInvertido (int *, int, char *);	// FUNCAO CRIADA PARA MOSTRAR O VETOR GERADO "ORDENADO AO CONTRARIO"
-void bubble (int *, int);	// FUN��O CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGOR�TMO "BUBBLE SORT"
-void bubbleSort (int *, int);	// FUN��O CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGOR�TMO "BUBBLE SORT"
-void insertionSort (int *, int);	// FUN��O CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGOR�TMO "INSERTION SORT"
-void selectionSort (int *, int);	// FUN��O CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGOR�TMO "SELECTION SORT"
-void troca (int *, int);	// FUN��O CRIADA PARA TROCAR A POSI��O E ORDENAR O VETOR "..."      // MELHORAR A DESCRI��O <<<<<<<<<<<	
-void trocaProximo (int *, int);	// FUN��O CRIADA PARA TROCAR A POSI��O E ORDENAR O VETOR "..."      // MELHORAR A DESCRI��O <<<<<<<<<<<
-void trocaAnterior (int *, int);	// FUN��O CRIADA PARA TROCAR A POSI��O E ORDENAR O VETOR "..."      // MELHORAR A DESCRI��O <<<<<<<<<<<
-void trocaMinimo (int *, int);	// FUN��O CRIADA PARA TROCAR A POSI��O E ORDENAR O VETOR "..."      // MELHORAR A DESCRI��O <<<<<<<<<<<
+void bubble (int *, int);	// FUNCAO CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGORITMO "BUBBLE SORT"
+void bubbleSort (int *, int);	// FUNCAO CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGORITMO "BUBBLE SORT"
+void insertionSort (int *, int);	// FUNCAO CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGORITMO "INSERTION SORT"
+void selectionSort (int *, int);	// FUNCAO CRIADA PARA ORDENAR O VETOR UTILIZANDO O ALGORITMO "SELECTION SORT"
+void troca (int *, int);	// FUNCAO CRIADA PARA TROCAR A POSICAO E ORDENAR O VETOR "..."
+void trocaProximo (int *, int);	// FUNCAO CRIADA PARA TROCAR A POSICAO E ORDENAR O VETOR "..."
+void trocaAnterior (int *, int);	// FUNCAO CRIADA PARA TROCAR A POSICAO E ORDENAR O VETOR "..."
+void trocaMinimo (int *, int);	// FUNCAO CRIADA PARA TROCAR A POSICAO E ORDENAR O VETOR "..."
 int buscaBinaria (int *, int, int);
 int tem_repeticao(int *, int , int);
+void gera_vetor_crescente(int *, int);
+void gera_vetor_decrescente(int *, int);
